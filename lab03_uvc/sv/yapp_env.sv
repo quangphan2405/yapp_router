da9d855341589b2d7dc971b5b96cd959d79e9ab7
quangphan2405 <quangphan240599@gmail.com>
2021-01-20 14:30:42 +0200

    Naming convention

--- lab03_uvc/sv/yapp_env.sv
+++ lab03_uvc/sv/yapp_env.sv
@@ -2,7 +2,7 @@ class yapp_env extends uvm_env;
 
    `uvm_component_utils(yapp_env)
 
-   yapp_tx_agent agent;
+   yapp_tx_agent tx_agent;
    
    function new(string name, uvm_component parent);
       super.new(name, parent);
@@ -10,7 +10,7 @@ class yapp_env extends uvm_env;
 
    virtual function void build_phase(uvm_phase phase);
       super.build_phase(phase);
-      agent = new("agent", this);
+      tx_agent = new("tx_agent", this);
    endfunction : build_phase
 
 endclass : yapp_env
