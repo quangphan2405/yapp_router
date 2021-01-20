70ff134967b0cde16ab20b04537e8fba7bf2decb
quangphan2405 <quangphan240599@gmail.com>
2021-01-21 00:58:48 +0200

    Modify inherited class's build phase

--- lab04_factory/tb/router_test_lib.sv
+++ lab04_factory/tb/router_test_lib.sv
@@ -13,11 +13,9 @@ class base_test extends uvm_test;
 
    // Build_phase method
    virtual function void build_phase(uvm_phase phase);
+      super.build_phase(phase);  
       uvm_config_int::set(this, "*", "recording_detail", 1);
-      super.build_phase(phase);      
-      `uvm_info("BUILD", "Build phase of test is being executed", UVM_HIGH)
-      uvm_config_wrapper::set(this, "tb.yapp.tx_agent.sequencer.run_phase",
-			      "default_sequence", yapp_5_packets::get_type());
+      `uvm_info("BUILD", "Build phase of test is being executed", UVM_HIGH)      
       tb = router_tb::type_id::create("tb", this);
    endfunction : build_phase
 
@@ -48,7 +46,7 @@ endclass : test2
 class short_packet_test extends base_test;
 
    // UVM component utility macro
-   `uvm_component_utils(base_test)
+   `uvm_component_utils(short_packet_test)
 
    // Constructor
    function new(string name, uvm_component parent);
@@ -57,22 +55,10 @@ class short_packet_test extends base_test;
 
    // Build_phase method
    virtual function void build_phase(uvm_phase phase);
-      // Enable transaction recording
-      uvm_config_int::set(this, "*", "recording_detail", 1);
-      
-      // Change packet type to short_yapp_packet
       set_type_override_by_type(yapp_packet::get_type(), short_yapp_packet::get_type());
-
-      // Build
       super.build_phase(phase);
-      
-      // Set sequencer to execute generated sequence
-      `uvm_info("BUILD", "Build phase of test is being executed", UVM_HIGH)
       uvm_config_wrapper::set(this, "tb.yapp.tx_agent.sequencer.run_phase",
 			      "default_sequence", yapp_5_packets::get_type());
-
-      // Create testbench instance
-      tb = router_tb::type_id::create("tb", this);
    endfunction : build_phase
 
 endclass : short_packet_test
