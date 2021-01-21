274ee3425018cad9328ddf33aaa56ce8f85418be
quangphan2405 <quangphan240599@gmail.com>
2021-01-21 16:36:02 +0200

    Change to tb_top module

--- lab06_vif/tb/tb_top.sv
+++ lab06_vif/tb/tb_top.sv
@@ -6,7 +6,7 @@ Notes         : From the Cadence "SystemVerilog Advanced Verification with UVM" 
 Copyright Cadence Design Systems (c)2015
 -----------------------------------------------------------------*/
 
-module top;
+module tb_top;
 // import the UVM library
 // include the UVM macros
    import uvm_pkg::*;
@@ -21,6 +21,9 @@ module top;
 // include the test library
 `include "router_test_lib.sv"
 
+   // set the interface
+   yapp_vif_config::set(null, "tb.yapp.agent.*", "vif", hw_top.in0);
+   
 initial begin
    run_test("base_test");
 end
