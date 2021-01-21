3b1c69d005bd24c7f7bb0da23212a22d8101a0cf
quangphan2405 <quangphan240599@gmail.com>
2021-01-21 19:33:24 +0200

    Delete unnecessary function

--- lab06_vif/tb/router_tb.sv
+++ lab06_vif/tb/router_tb.sv
@@ -8,14 +8,9 @@ class router_tb extends uvm_env;
       super.new(name, parent);
    endfunction : new
 
-   virtual function void build_phase(uvm_phase phase);
+   function void build_phase(uvm_phase phase);
       super.build_phase(phase);
-      `uvm_info("BUILD", "Build phase of tb is being executed", UVM_HIGH)
       yapp = yapp_env::type_id::create("yapp", this);
    endfunction : build_phase
-
-   function void start_of_simulation_phase(uvm_phase phase);
-      `uvm_info(get_type_name(), {"Simulation starts for: ", get_full_name()}, UVM_HIGH)
-   endfunction : start_of_simulation_phase
 
 endclass : router_tb
