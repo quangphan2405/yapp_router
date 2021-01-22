3d7d774a999cefbf085fa33d92d682ddb1901a01
quangphan2405 <quangphan240599@gmail.com>
2021-01-22 15:49:07 +0200

    Create DUT instance and map port

--- lab06_vif/tb/hw_top.sv
+++ lab06_vif/tb/hw_top.sv
@@ -11,29 +11,58 @@ Copyright Cadence Design Systems (c)2015
 
 module hw_top;
 
-  // Clock and reset signals
-  logic [31:0]  clock_period;
-  logic         run_clock;
-  logic         clock;
-  logic         reset;
+   // Clock and reset signals
+   logic [31:0]  clock_period;
+   logic         run_clock;
+   logic         clock;
+   logic         reset;
 
-  // YAPP Interface to the DUT
-  yapp_if in0(clock, reset);
+   // YAPP Interface to the DUT
+   yapp_if in0(clock, reset);
 
-  // CLKGEN module generates clock
-  clkgen clkgen (
-    .clock(clock),
-    .run_clock(1'b1),
-    .clock_period(32'd10)
-  );
+   // CLKGEN module generates clock
+   clkgen clkgen (
+		  .clock(clock),
+		  .run_clock(1'b1),
+		  .clock_period(32'd10)
+		  );
 
-  initial begin
-    reset <= 1'b0;
-    in0.in_suspend <= 1'b0;
-    @(negedge clock)
-      #1 reset <= 1'b1;
-    @(negedge clock)
-      #1 reset <= 1'b0;
-  end
+   yapp_router dut(
+		   .reset(reset),
+		   .clock(clock),
+		   .error(),
+
+		   // YAPP interface
+		   .in_data(in0.data),
+		   .in_data_vld(in0.data_vld),
+		   .in_suspend(in0.data_suspend),
+
+		   // Output Channels
+		   //Channel 0
+		   .data_0(),
+		   .data_vld_0(),
+		   .suspend_0(1'b0),
+		   //Channel 1
+		   .data_1(),
+		   .data_vld_1(),
+		   .suspend_1(1'b0),
+		   //Channel 2
+		   .data_2(),
+		   .data_vld_2(),
+		   .suspend_2(1'b0),
+
+		   // HBUS Interface 
+		   .haddr(),
+		   .hdata(),
+		   .hen(),
+		   .hwr_rd());
+
+   initial begin
+      reset <= 1'b0;
+      @(negedge clock)
+	#1 reset <= 1'b1;
+      @(negedge clock)
+	#1 reset <= 1'b0;
+   end
 
 endmodule
