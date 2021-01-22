1f83289357203260aa9d1749ec38fbad1f68cd75
quangphan2405 <quangphan240599@gmail.com>
2021-01-22 18:47:39 +0200

    Add clock_and_rst UVC to testbench

--- lab07_integ/tb/router_tb.sv
+++ lab07_integ/tb/router_tb.sv
@@ -12,23 +12,29 @@ class router_tb extends uvm_env;
 
    // HBUS UVC environment handle
    hbus_env hbus;
-         
+
+   // Clock and Reset UVC environment handle
+   clock_and_reset_env clk_rst;
+            
    function new(string name, uvm_component parent);
       super.new(name, parent);
    endfunction : new
 
    function void build_phase(uvm_phase phase);
       super.build_phase(phase);
-      // Create YAPP UVC instances
-      yapp  = yapp_env::type_id::create("yapp", this);
+      // Create YAPP UVC instance
+      yapp = yapp_env::type_id::create("yapp", this);
 
       // Create channel UVC instances
       chan0 = channel_env::type_id::create("chan0", this);
       chan1 = channel_env::type_id::create("chan1", this);
       chan2 = channel_env::type_id::create("chan2", this);
 
-      // Create HBUS UVC instances
-      hbus  = hbus_env::type_id::create("hbus", this);
+      // Create HBUS UVC instance
+      hbus = hbus_env::type_id::create("hbus", this);
+
+      // Create clock_and_reset UVC instance
+      clk_rst = hbus_env::type_id::create("clk_rst", this);
       
       // Configuration set channel_id of channels
       uvm_config_int::set(this, "chan0", "channel_id", 0);
