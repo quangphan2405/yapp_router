ea61d23ed5acd74f9ba9bb90436d8580c8e573b2
quangphan2405 <quangphan240599@gmail.com>
2021-01-22 18:44:31 +0200

    Add HBUS UVC to testbench

--- lab07_integ/tb/router_tb.sv
+++ lab07_integ/tb/router_tb.sv
@@ -9,40 +9,35 @@ class router_tb extends uvm_env;
    channel_env chan0;
    channel_env chan1;
    channel_env chan2;
-      
+
+   // HBUS UVC environment handle
+   hbus_env hbus;
+         
    function new(string name, uvm_component parent);
       super.new(name, parent);
    endfunction : new
 
    function void build_phase(uvm_phase phase);
       super.build_phase(phase);
-      // Create YAPP UVC environment
+      // Create YAPP UVC instances
       yapp  = yapp_env::type_id::create("yapp", this);
 
-      // Create channel UVC environment
+      // Create channel UVC instances
       chan0 = channel_env::type_id::create("chan0", this);
       chan1 = channel_env::type_id::create("chan1", this);
       chan2 = channel_env::type_id::create("chan2", this);
 
-      // Configuration set channel_id
+      // Create HBUS UVC instances
+      hbus  = hbus_env::type_id::create("hbus", this);
+      
+      // Configuration set channel_id of channels
       uvm_config_int::set(this, "chan0", "channel_id", 0);
       uvm_config_int::set(this, "chan1", "channel_id", 1);
       uvm_config_int::set(this, "chan2", "channel_id", 2);
-   endfunction : build_phase
 
-endclass : router_tbclass router_tb extends uvm_env;
-
-   `uvm_component_utils(router_tb)
-
-   yapp_env yapp;
-   
-   function new(string name, uvm_component parent);
-      super.new(name, parent);
-   endfunction : new
-
-   function void build_phase(uvm_phase phase);
-      super.build_phase(phase);
-      yapp = yapp_env::type_id::create("yapp", this);
+      // Configuration set num_masters and num_slaves of hbus
+      uvm_config_int::set(this, "hbus", "num_masters", 1);
+      uvm_config_int::set(this, "hbus", "num_slaves", 0);
    endfunction : build_phase
 
 endclass : router_tb
