8bf1dcccec63d8fc84529221f61fa0e21ec289f5
quangphan2405 <quangphan240599@gmail.com>
2021-01-23 12:16:01 +0200

    Lab07 run file

--- lab07_integ/tb/run.f
+++ lab07_integ/tb/run.f
@@ -13,15 +13,26 @@ Copyright Cadence Design Systems (c)2015
 
 // include directories
 //*** add incdir include directories here
--incdir ../sv // include directory for sv files
+// -incdir ../sv // include directory for sv files
+-incdir ../../yapp/sv            // incude directory for YAPP UVC
+-incdir ../../channel/sv         // include directory for channel UVC
+-incdir ../../hbus/sv            // include directory for HBUS UVC
+-incdir ../../clock_and_reset/sv // include directory for clock_and_reset UVC
 
 // set default timescale
 -timescale 1ns/1ns
 
 // compile files
 //*** add compile files here
-../sv/yapp_pkg.sv // compile YAPP package
-../sv/yapp_if.sv  // compile YAPP interface
+../../yapp/sv/yapp_pkg.sv 			// compile YAPP package
+../../yapp/sv/yapp_if.sv  			// compile YAPP interface
+../../channel/sv/channel_pkg.sv 		// compile channel package
+../../channel/sv/channel_if.sv 			// compile channel interface
+../../hbus/sv/hbus_pkg.sv 			// compile HBUS package
+../../hbus/sv/hbus_if.sv 			// compile HBUS interface
+../../clock_and_reset/sv/clock_and_reset_pkg.sv // compile clock_and_reset package
+../../clock_and_reset/sv/clock_and_reset_if.sv  // compile clock_and_reset interface
+
 clkgen.sv // compile clock generation
 ../../router_rtl/yapp_router.sv // compile yapp router
 hw_top.sv // compile hardware top level
